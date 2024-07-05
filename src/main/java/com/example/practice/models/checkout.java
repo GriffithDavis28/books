d9bf9d5aabd68da950037566426fba988b516249
Griffith <davis.griffith@phenom.com>
2024-07-05 15:46:54 +0530

    optimised checkout

--- src/main/java/com/example/practice/models/checkout.java
+++ src/main/java/com/example/practice/models/checkout.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -11,37 +12,30 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import lombok.Getter;
 import lombok.Setter;
 
-
 @Getter
 @Setter
 @Document("checkout")
 public class checkout {
-    
+
     @Id
     private String id;
 
     private String full_name;
     private List<CheckoutDetails> details = new ArrayList<>();
-    
 
     public checkout(String id, Users user, List<CheckoutDetails> details) {
         this.id = id;
-        this.details = details;
-        
-        if(user != null){
-            this.full_name = user.getFirstName() + " " + user.getLastName();
-        }
+        this.details = details != null ? details : List.of();
+        this.full_name = user != null ? user.getFirstName() + " " + user.getLastName() : null;
     }
 
-    public List<Map<String, Object>> getDetails(){
-
-        List<Map<String, Object>> refinedList =  new ArrayList<>();
-        for (CheckoutDetails detail : details){
-            Map<String, Object> refinedMap = new HashMap<>();
-            refinedMap.put("Book_Name", detail.getBookName());
-            refinedMap.put("ISBN", detail.getIsbn());
-            refinedList.add(refinedMap);
-        }
-        return refinedList;
+    public List<Map<String, Object>> getDetails() {
+        return details.stream()
+                .map(detail -> {
+                    Map<String, Object> refinedMap = new HashMap<>();
+                    refinedMap.put("Book_Name", detail.getBookName());
+                    refinedMap.put("ISBN", detail.getIsbn());
+                    return refinedMap;
+                }).collect(Collectors.toList());
     }
 }
